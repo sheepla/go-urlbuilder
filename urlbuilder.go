4cfe2e6f27f2bd03e82a71ec1fba9d7577bcee42
sheepla <62412884+sheepla@users.noreply.github.com>
2023-10-22 12:18:48 +0900

    Go fmt

--- urlbuilder.go
+++ urlbuilder.go
@@ -7,42 +7,41 @@ import (
 
 type URL struct {
 	internal *url.URL
-    err error
+	err      error
 }
 
 func (u *URL) Error() string {
-    return u.err.Error()
+	return u.err.Error()
 }
 
 func Parse(s string) (*URL, error) {
 	netURL, err := url.Parse(s)
-    if err != nil {
-        return &URL{
-            internal: netURL,
-            err: err,
-        }, err
-    }
+	if err != nil {
+		return &URL{
+			internal: netURL,
+			err:      err,
+		}, err
+	}
 
-    return &URL{
-        internal: netURL,
-        err: nil,
-    }, nil
+	return &URL{
+		internal: netURL,
+		err:      nil,
+	}, nil
 }
 
 func (u *URL) SetPath(base string, elements ...string) *URL {
 	path, err := url.JoinPath(base, elements...)
-    u.err = err
+	u.err = err
 	u.internal.Path = path
 
-    return u
+	return u
 }
-
 
 func (u *URL) EditPath(editFunc func([]string) []string) *URL {
 	elements := strings.Split(u.internal.Path, "/")
 	elements = editFunc(elements)
 	path, err := url.JoinPath("/", elements...)
-    u.err = err
+	u.err = err
 	u.internal.Path = path
 
 	return u
@@ -61,14 +60,14 @@ func (u *URL) SetHost(host string) *URL {
 }
 
 func (u *URL) SetFragument(fragument string) *URL {
-    u.internal.Fragment = fragument
+	u.internal.Fragment = fragument
 
 	return u
 }
 
 func (u *URL) SetQuery(key, value string) *URL {
 	q, err := url.ParseQuery(u.internal.RawQuery)
-    u.err = err
+	u.err = err
 	q.Set(key, value)
 	u.internal.RawQuery = q.Encode()
 
@@ -77,17 +76,16 @@ func (u *URL) SetQuery(key, value string) *URL {
 
 func (u *URL) AddQuery(key, value string) *URL {
 	q, err := url.ParseQuery(u.internal.RawQuery)
-    u.err = err
+	u.err = err
 	q.Add(key, value)
 	u.internal.RawQuery = q.Encode()
 
 	return u
 }
 
-
 func (u *URL) RemoveQuery(key string) *URL {
 	q, err := url.ParseQuery(u.internal.RawQuery)
-    u.err = err
+	u.err = err
 	q.Del(key)
 	u.internal.RawQuery = q.Encode()
 
@@ -95,14 +93,14 @@ func (u *URL) RemoveQuery(key string) *URL {
 }
 
 func (u *URL) String() (string, error) {
-    return u.internal.String(), u.err
+	return u.internal.String(), u.err
 }
 
 func (u *URL) MustString() string {
-    s, err := u.String()
-    if err != nil {
-        panic(err)
-    }
+	s, err := u.String()
+	if err != nil {
+		panic(err)
+	}
 
-    return s
+	return s
 }
--- urlbuilder_test.go
+++ urlbuilder_test.go
@@ -6,30 +6,30 @@ import "github.com/sheepla/urlbuilder"
 var exampleURL = "https://localhost:8080/path/to/resource#helloworld?key1=value1&key2=value2"
 
 func Test(t *testing.T) {
-    u, err := urlbuilder.Parse(exampleURL)
-    if err != nil {
-        t.Fatal(err)
-    }
+	u, err := urlbuilder.Parse(exampleURL)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    u.SetScheme("ftp").
-        SetHost("another.example.com:12345").
-        SetFragument("anotherFragument").
-        EditPath(func(elements []string)[]string {
-            sl := []string{"日本語", "with space"}
-            elements = append(elements, sl...)
+	u.SetScheme("ftp").
+		SetHost("another.example.com:12345").
+		SetFragument("anotherFragument").
+		EditPath(func(elements []string) []string {
+			sl := []string{"日本語", "with space"}
+			elements = append(elements, sl...)
 
-            return elements
-        }).
-        AddQuery("key3", "key3").
-        RemoveQuery("key2")
+			return elements
+		}).
+		AddQuery("key3", "key3").
+		RemoveQuery("key2")
 
-    have, err := u.String()
-    if err != nil {
-        t.Fatal(err)
-    }
+	have, err := u.String()
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    want := "ftp://another.example.com:12345/path/to/resource/%25E6%2597%25A5%25E6%259C%25AC%25E8%25AA%259E/with%2520space?key3=key3#anotherFragument"
-    if have != want {
-        t.Fatalf("have=%s\nwant=%s\n", have, want)
-    }
+	want := "ftp://another.example.com:12345/path/to/resource/%25E6%2597%25A5%25E6%259C%25AC%25E8%25AA%259E/with%2520space?key3=key3#anotherFragument"
+	if have != want {
+		t.Fatalf("have=%s\nwant=%s\n", have, want)
+	}
 }
