83229126be96e51215f8a755ab3277e730489b3c
sheepla <62412884+sheepla@users.noreply.github.com>
2023-10-22 17:01:21 +0900

    Fix example.go

--- example/example.go
+++ example/example.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 
 	"github.com/sheepla/go-urlbuilder"
 )
@@ -9,23 +10,25 @@ import (
 var sourceURL = "https://localhost:8080/path/to/resource#helloworld?key1=value1&key2=value2"
 
 func main() {
-	ExampleURLBuilder()
-}
-
-func ExampleURLBuilder() {
 	u, err := urlbuilder.Parse(sourceURL)
 	if err != nil {
 		panic(err)
 	}
 
-	u.SetScheme("ftp").
-		SetHost("another.example.com:12345").
+	u.SetScheme("http").
+		SetHost("example.com:12345").
 		SetFragument("anotherFragument").
-        SetPath("/", "日本語", "with space").
-		AddQuery("key3", "key3").
-		RemoveQuery("key2")
+		EditPath(func(elements []string) []string {
+			return append(elements, "Go言語")
+		}).
+		EditQuery(func(q url.Values) url.Values {
+			q.Set("key1", "key1-edited")
+			q.Del("key2")
+			q.Add("key3", "value3")
 
-	// Will output:
-	// ftp://another.example.com:12345/%25E6%2597%25A5%25E6%259C%25AC%25E8%25AA%259E/with%2520space?key3=key3#anotherFragument
+			return q
+		})
+
+		// => http://example.com:12345/path/to/resource/Go%25E8%25A8%2580%25E8%25AA%259E?key1=key1-edited&key3=value3#anotherFragument
 	fmt.Println(u.MustString())
 }
